3712cef5a91b09244eedacc2aa9cfdf3d25667a5
Pedro Arthur Barbosa Carvalho <pedro.barbosa@zup.com.br>
2021-10-08 17:22:42 -0300

    Piloto Pedro copiloto Marcelo e Everton

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into conta-digital.conta (id, numero, id-cliente, saldo) values (null, 8101, 1, 0.0);
+insert into conta-digital.conta (id, numero, id-cliente, saldo) values (null, 8102, 2, 10.0);
+insert into conta-digital.conta (id, numero, id-cliente, saldo) values (null, 8103, 3, 100.0);
+
+
+insert into conta-digital.cliente (id, nome) values (null, "Marcelo");
+insert into conta-digital.cliente (id, numero, cliente, saldo) values (null, "Pedro");
+insert into conta-digital.cliente (id, numero, cliente, saldo) values (null, "Everton");
