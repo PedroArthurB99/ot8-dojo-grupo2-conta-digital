d5e74763d5ca426ea9b841c238d84faeb2685ce1
Everton-ZUP <everton.fabris@zup.com.br>
2021-10-15 15:40:57 -0300

    Piloto Everton copiloto Marcelo Pedro Vinicius

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,5 @@
-insert into conta-digital.conta (id, numero, id-cliente, saldo) values (null, 8101, 1, 0.0);
-insert into conta-digital.conta (id, numero, id-cliente, saldo) values (null, 8102, 2, 10.0);
-insert into conta-digital.conta (id, numero, id-cliente, saldo) values (null, 8103, 3, 100.0);
+insert into conta-digital.cliente (id, nome) values (null, "Marcelo"), (null, "Pedro"),(null, "Everton"),(null, "Vinicius");
 
+insert into conta-digital.conta (id, numero, cliente_id, saldo)
+values (null, 8101, 1, 0.0),(null, 8102, 2, 10.0),(null, 8103, 3, 100.0);
 
-insert into conta-digital.cliente (id, nome) values (null, "Marcelo");
-insert into conta-digital.cliente (id, numero, cliente, saldo) values (null, "Pedro");
-insert into conta-digital.cliente (id, numero, cliente, saldo) values (null, "Everton");
