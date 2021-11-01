59b6d15cf1ea8bd39a641dffd019c9b3ad08f4de
Marcelo Gomes <marcelo.gomes@zup.com.br>
2021-11-01 16:53:55 -0300

    Consulta valor de saldo - Boolean

--- src/main/java/br/com/orange/contadigital/conta/ContaController.java
+++ src/main/java/br/com/orange/contadigital/conta/ContaController.java
@@ -10,9 +10,10 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
+import java.math.BigDecimal;
 
 @RestController
-@RequestMapping("contas")
+@RequestMapping("/contas")
 public class ContaController {
 
     @Autowired
@@ -27,7 +28,7 @@ public class ContaController {
                 "Não existe uma conta com esse id.")));
         conta.deposito(form.getValor());
         contaRepository.save(conta);
-        logger.info("Depósito realizado com sucesso no valor de: "+form.getValor()+" na conta: "+conta.getId());
+        logger.info("Depósito realizado com sucesso no valor de: " + form.getValor() + " na conta: " + conta.getId());
         return "Seu saldo atualizado é " + conta.getSaldo();
     }
 
@@ -38,7 +39,21 @@ public class ContaController {
                 "Não existe uma conta com esse id.")));
         conta.saque(form.getValor());
         contaRepository.save(conta);
-        logger.info("Saque realizado com sucesso no valor de: "+form.getValor()+" na conta: "+conta.getId());
+        logger.info("Saque realizado com sucesso no valor de: " + form.getValor() + " na conta: " + conta.getId());
         return "Seu saldo atualizado é " + conta.getSaldo();
+    }
+
+    @GetMapping("/{id}/saldo")
+    public Boolean saldo(@PathVariable("id") Long id, @RequestBody @Valid OperacaoForm form) {
+        ContaDigital conta = this.contaRepository.findById(id).orElseThrow(() -> new RecursoNaoEncontradoException(new ObjetoErroDTO("id",
+                "Não existe uma conta com esse id.")));
+
+        BigDecimal valor = form.getValor();
+
+        if (conta.getSaldo().compareTo(valor)>=0){
+            return true;
+        }
+        return false;
+
     }
 }
